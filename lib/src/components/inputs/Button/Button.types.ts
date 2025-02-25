a68785e8a2170f6b10bfe37d11aa8f3b2f1730c3
khaledtag98 <khaledtageldeen98@gmail.com>
2025-02-25 02:44:21 +0200

    refactor: simplify Button component types by removing unused valid elements and comments

--- lib/src/components/inputs/Button/Button.types.ts
+++ lib/src/components/inputs/Button/Button.types.ts
@@ -3,34 +3,51 @@ import type { VariantProps } from "class-variance-authority";
 import { buttonVariants } from "./variants";
 import { PolymorphicProps } from "@/utils/PolymorphicComponent";
 
-// Define valid HTML elements that can be used as a button
-export type ValidButtonElements =
-  | "button"
-  | "a"
-  | "div"
-  | "span"
-  | "input"
-  | "label"
-  | "select"
-  | "textarea"
-  | "form";
+// export interface UseButtonParameters {
+//   /**
+//    * If `true`, the component is disabled.
+//    * @default false
+//    */
+//   disabled?: boolean;
+//   /**
+//    * If `true`, allows a disabled button to receive focus.
+//    * @default false
+//    */
+//   focusableWhenDisabled?: boolean;
+//   href?: string;
+//   onFocusVisible?: React.FocusEventHandler;
+//   rootRef?: React.Ref<Element>;
+//   tabIndex?: NonNullable<React.HTMLAttributes<any>["tabIndex"]>;
+//   to?: string;
+//   /**
+//    * Type attribute applied when the `component` is `button`.
+//    * @default 'button'
+//    */
+//   type?: React.ButtonHTMLAttributes<HTMLButtonElement>["type"];
+//   /**
+//    * The HTML element, e.g.'button', 'a' etc
+//    * @default ''
+//    */
+//   rootElementName?: keyof HTMLElementTagNameMap;
+// }
 
 export interface ButtonTypeMap<
   AdditionalProps = object,
-  RootComponentType extends React.ElementType = ValidButtonElements,
+  RootComponentType extends React.ElementType = "button",
 > {
   props: ButtonOwnProps & AdditionalProps;
   defaultComponent: RootComponentType;
 }
 
 export type ButtonOwnProps<
-  RootComponentType extends React.ElementType = ValidButtonElements,
+  RootComponentType extends React.ElementType = "button",
 > = {
   loading?: boolean;
   loadingPosition?: "start" | "center" | "end";
   loadingIndicator?: React.ReactNode;
   startIcon?: React.ReactNode;
   endIcon?: React.ReactNode;
+  rootElementName?: keyof HTMLElementTagNameMap;
   component?: RootComponentType;
 } & VariantProps<typeof buttonVariants>;
 
