56826574d7743a828b90e9d8efafb86c5a4af573
wibrahim.c <wibrahim.c>
2025-05-13 13:25:36 +0300

    test: add color and underline test on link

--- lib/src/components/navigations/Link/Link.cy.tsx
+++ lib/src/components/navigations/Link/Link.cy.tsx
@@ -8,6 +8,36 @@ describe("Link Component", () => {
       .and("have.attr", "href", "#")
       .and("contain.text", "Link");
   });
+  // Test with text decoration class
+  it("renders with different text decorations", () => {
+    // Test always underline
+    cy.mount(<Link underline="always">Link underline</Link>);
+    cy.get("a").should("have.class", "underline");
+
+    // Test hover underline
+    cy.mount(<Link underline="hover">hover underline</Link>);
+    cy.get("a").should("have.class", "hover:underline");
+    // Test no underline
+    cy.mount(<Link underline="none">none underline</Link>);
+    cy.get("a").should("have.class", "no-underline");
+  });
+  // Test with different colors
+  it("renders with different colors", () => {
+    cy.mount(<Link color="primary">Primary</Link>);
+    cy.get("a").should("exist");
+
+    cy.mount(<Link color="secondary">Secondary</Link>);
+    cy.get("a").should("exist");
+
+    cy.mount(<Link color="success">Success</Link>);
+    cy.get("a").should("exist");
+
+    cy.mount(<Link color="warning">Warning</Link>);
+    cy.get("a").should("exist");
+
+    cy.mount(<Link color="error">Error</Link>);
+    cy.get("a").should("exist");
+  });
 
   it("renders as a Button", () => {
     cy.mount(
