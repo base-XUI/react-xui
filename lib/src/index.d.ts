6377a7669aa6426a226af1b206e1429eabfd7dee
mallam.c <mallam.c@saip.sa>
2025-05-23 04:56:44 +0300

    fix: remove FOrmControlLabel Path

--- lib/src/index.d.ts
+++ lib/src/index.d.ts
@@ -1,5 +1,4 @@
 export * from "./components/inputs/Button";
 export * from "./components/system-design/Typography";
 export * from "./components/Checkbox/Checkbox";
-export * from "./components/API/FormControlLabel/FormControlLabel";
 export * from "./components/API/FormGroup/FormGroup";
--- lib/src/index.ts
+++ lib/src/index.ts
@@ -3,6 +3,5 @@ import "./main.css";
 export * from "./components/inputs/Button";
 export * from "./components/system-design/Typography";
 export * from "./components/Checkbox/Checkbox";
-export * from "./components/API/FormControlLabel/FormControlLabel";
 export * from "./components/API/FormGroup/FormGroup";
 export * from "./utils/polymorphic";
