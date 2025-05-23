6883ec474e82149e1e45d08d0ec29d101a212043
mallam.c <mallam.c@saip.sa>
2025-05-23 04:05:03 +0300

    fix: export file

--- lib/src/index.d.ts
+++ lib/src/index.d.ts
@@ -1,4 +1,5 @@
 export * from "./components/inputs/Button";
 export * from "./components/system-design/Typography";
 export * from "./components/Checkbox/Checkbox";
+export * from "./components/API/FormControlLabel/FormControlLabel";
 export * from "./components/API/FormGroup/FormGroup";
--- lib/src/index.ts
+++ lib/src/index.ts
@@ -3,5 +3,6 @@ import "./main.css";
 export * from "./components/inputs/Button";
 export * from "./components/system-design/Typography";
 export * from "./components/Checkbox/Checkbox";
+export * from "./components/API/FormControlLabel/FormControlLabel";
 export * from "./components/API/FormGroup/FormGroup";
 export * from "./utils/polymorphic";
