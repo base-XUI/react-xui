2ca3148d9fae5049e851718f531bdf4cbdff95e2
mmostafa.c <mostafatarekaioup@gmail.com>
2025-05-25 12:14:01 +0300

    export snackbar component

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -1,5 +1,6 @@
 import "./main.css";
 
 export * from "./components/inputs/Button";
+export * from "./components/feedback/Snackbar";
 export * from "./components/system-design/Typography";
 export * from "./utils/polymorphic";
