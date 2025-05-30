8ba507a369625cda824703ac0bc249bcb5e13534
mallam.c <mallam.c@saip.sa>
2025-05-30 20:55:30 +0300

    feat:  radio component path

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -3,5 +3,5 @@ import "./main.css";
 export * from "./components/inputs/Button";
 export * from "./components/system-design/Typography";
 export * from "./components/Checkbox/Checkbox";
-export * from "./components/API/FormGroup/FormGroup";
+export * from "./components/API/Radio/Radio";
 export * from "./utils/polymorphic";
