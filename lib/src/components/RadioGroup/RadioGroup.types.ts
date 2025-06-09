eadfc4bf9b3abe95b05287bc4f155754fddebee1
mallam.c <mallam.c@saip.sa>
2025-06-10 01:13:53 +0300

    feat: RadioGroup Types file

---
+++ lib/src/components/RadioGroup/RadioGroup.types.ts
@@ -0,0 +1,32 @@
+import type { ReactNode, CSSProperties, ChangeEvent } from "react";
+
+export interface RadioChangeEvent
+  extends Omit<ChangeEvent<HTMLInputElement>, "target"> {
+  target: ChangeEvent<HTMLInputElement>["target"] & {
+    value: Primitive;
+  };
+}
+export type Primitive = string | number | boolean;
+
+export interface RadioChangeMetadata {
+  id: string;
+  value: Primitive;
+  name: string;
+  checked: boolean;
+}
+
+export interface RadioGroupProps {
+  id?: string;
+  children?: ReactNode;
+  defaultValue?: Primitive;
+  value?: any;
+  onChange?: (
+    event: ChangeEvent<HTMLInputElement>,
+    value?: Primitive,
+    meta?: RadioChangeMetadata,
+  ) => void;
+  onValueChange?: (value: Primitive, meta?: RadioChangeMetadata) => void;
+  row?: boolean;
+  sx?: CSSProperties;
+  name?: string;
+}
