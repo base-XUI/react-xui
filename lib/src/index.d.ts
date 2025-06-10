c391092ae76a43fee175de41c7e791e4297adfa0
mallam.c <mallam.c@saip.sa>
2025-06-10 07:23:51 +0300

    fix: remove export of formControlLabel

--- lib/src/index.d.ts
+++ lib/src/index.d.ts
@@ -3,4 +3,3 @@ export * from "./components/system-design/Typography";
 export * from "./components/Checkbox/Checkbox";
 export * from "./components/API/Radio/Radio";
 export * from "./components/RadioGroup/RadioGroup";
-export * from "./components/API/FormControlLabel/FormControlLabel";
--- lib/src/index.ts
+++ lib/src/index.ts
@@ -5,5 +5,4 @@ export * from "./components/system-design/Typography";
 export * from "./components/Checkbox/Checkbox";
 export * from "./components/API/Radio/Radio";
 export * from "./components/RadioGroup/RadioGroup";
-export * from "./components/API/FormControlLabel/FormControlLabel";
 export * from "./utils/polymorphic";
