596d3a165ab000895afe9dd9855222fcf95d23c5
mallam.c <mallam.c@saip.sa>
2025-06-17 11:26:57 +0300

    feat: FormControlLabel variant file

---
+++ lib/src/components/API/FormControlLabel/variants.ts
@@ -0,0 +1,8 @@
+export const ALLOWED_COMPONENT_NAMES = ["Checkbox", "Radio", "Switch"];
+export const VALID_INPUT_TYPES = ["checkbox", "radio"];
+export const labelPlacementClasses = {
+  start: "flex-row",
+  end: "flex-row-reverse",
+  top: "flex-col-reverse",
+  bottom: "flex-col",
+};
