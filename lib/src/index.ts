8f7d3e98bc7b79f3356d8549bdd8c88b6b79efdb
otag.c <omar.tag@wscdev.net>
2025-06-18 15:49:26 +0300

    fix: Correct import path for Tabs component from system-design to surfaces

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -2,5 +2,5 @@ import "./main.css";
 
 export * from "./components/inputs/Button";
 export * from "./components/system-design/Typography";
-export * from "./components/system-design/Tabs";
+export * from "./components/surfaces/Tabs";
 export * from "./utils/polymorphic";
