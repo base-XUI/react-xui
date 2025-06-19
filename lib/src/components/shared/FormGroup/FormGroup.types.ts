650de254cbf45acad014955b5fe63bf7a957e881
mallam.c <mallam.c@saip.sa>
2025-06-19 13:12:35 +0300

    fix: remove comments and change interface by type

--- lib/src/components/shared/FormGroup/FormGroup.types.ts
+++ lib/src/components/shared/FormGroup/FormGroup.types.ts
@@ -1,39 +1,9 @@
-import type { ReactNode } from "react";
+import type { CSSProperties, ReactNode } from "react";
 
-/**
- * @file
- * Type definitions for the FormGroup component.
- * This file defines all props accepted by the FormGroup component.
- */
-
-/**
- * Props accepted by the FormGroup component.
- */
-export interface FormGroupProps {
-  /**
-   * The content of the component.
-   * Usually Checkbox or Radio elements.
-   */
+export type FormGroupProps = {
   children?: ReactNode;
-
-  /**
-   * Display group of elements in a compact row layout.
-   * @default false
-   */
   row?: boolean;
-
-  /**
-   * Optional additional class names for custom styling.
-   */
   className?: string;
-
-  /**
-   * Inline styles to customize the component appearance.
-   */
-  sx?: React.CSSProperties;
-
-  /**
-   * If true, the group will active the row animation.
-   */
+  sx?: CSSProperties;
   animation?: boolean;
-}
+};
